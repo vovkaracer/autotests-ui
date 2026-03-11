0bcb0d8ffcd4f7e0987fd1d05d4c5bd447e6b6c7
vovkaracer <vovka_racer@mail.ru>
2026-03-11 18:26:44 +0700

    Фикстуры Pytest

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import sync_playwright, Page, Playwright
+import pytest
+
+@pytest.fixture()
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
