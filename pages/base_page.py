572a4a4a246c8835b692b7d44f8aed31b1a07a1e
vovkaracer <vovka_racer@mail.ru>
2026-03-19 11:06:08 +0700

    Знакомство с PageObject

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import Page
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def visit(self, url: str):
+        self.page.goto(url)
+
+    def reload(self):
+        self.page.reload(wait_until='domcontentloaded')
