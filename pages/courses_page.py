10bf79266625950880f891050968223dd4405295
vovkaracer <vovka_racer@mail.ru>
2026-03-20 09:18:08 +0700

    Практикуемся в реализации PageObject часть 1

---
+++ pages/courses_page.py
@@ -0,0 +1,43 @@
+from base_page import BasePage
+from playwright.sync_api import Page, expect
+
+class CoursesPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
+
+        self.course_title = page.get_by_test_id('course-widget-title-text')
+        self.course_image = page.get_by_test_id('course-preview-image')
+        self.course_max_text = page.get_by_test_id('course-max-score-info-row-view-text')
+        self.course_min_text = page.get_by_test_id('course-min-score-info-row-view-text')
+        self.course_estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
+
+        self.course_menu_button = page.get_by_test_id('course-view-menu-button')
+        self.course_edit_menu_button = page.get_by_test_id('course-view-edit-menu-item')
+        self.course_delete_menu_button = page.get_by_test_id('course-view-delete-menu-item')
+
+        self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+        self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+        self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+
+    def check_visible_course_title(self):
+        expect(self.courses_title).to_be_visible()
+        expect(self.courses_title).to_have_text('Courses')
+
+    def check_visible_empty_view(self):
+        expect(self.empty_view_icon).to_be_visible()
+
+        expect(self.empty_view_title).to_be_visible()
+        expect(self.empty_view_title).to_have_text('There is no results')
+
+        expect(self.empty_view_description).to_be_visible()
+        expect(self.empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
+
+    def check_visible_create_course_button(self):
+        expect(self.create_course_button).to_be_visible()
+
+    def click_create_course_button(self):
+        self.create_course_button.click()
+
