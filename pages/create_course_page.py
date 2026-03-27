04b9dd42f85c49ac0bb2c6584d0fbf2527c29555
vovkaracer <vovka_racer@mail.ru>
2026-03-27 09:34:30 +0700

    Практикуемся в реализации PageObject небольшие дополнения

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -5,14 +5,17 @@ class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
+        # Заголовок и кнопка создания курса
         self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
 
+        # Картинка предпросмотра и блок предпросмотра картинки курса
         self.preview_image = page.get_by_test_id('create-course-preview-image-upload-widget-preview-image')
         self.preview_empty_course_icon = page.get_by_test_id('create-course-preview-empty-view-icon')
         self.preview_empty_course_title = page.get_by_test_id('create-course-preview-empty-view-title-text')
         self.preview_empty_course_description = page.get_by_test_id('create-course-preview-empty-view-description-text')
 
+        # Кнопка загрузки, удаления картинки предпросмотра курса и блок с информацией о загружаемой картинке
         self.preview_image_upload_icon = page.get_by_test_id('create-course-preview-image-upload-widget-info-icon')
         self.preview_image_upload_text = page.get_by_test_id(
             'create-course-preview-image-upload-widget-info-title-text'
@@ -27,6 +30,8 @@ class CreateCoursePage(BasePage):
             'create-course-preview-image-upload-widget-remove-button'
         )
         self.preview_image_upload_input = page.get_by_test_id('create-course-preview-image-upload-widget-input')
+
+        # Форма создания курса
         self.create_course_title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
         self.create_course_estimated_time_input = (
             page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
@@ -37,9 +42,11 @@ class CreateCoursePage(BasePage):
         self.create_course_max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
         self.create_course_min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
 
+        # Заголовок и кнопка создания задания
         self.exercises_title = page.get_by_test_id('create-course-exercises-box-toolbar-title-text')
         self.create_exercise_button = page.get_by_test_id('create-course-exercises-box-toolbar-create-exercise-button')
 
+        # Блок, который отображется, когда в курсе нет заданий
         self.exercise_empty_view_icon = page.get_by_test_id('create-course-exercises-empty-view-icon')
         self.exercise_empty_view_title = page.get_by_test_id('create-course-exercises-empty-view-title-text')
         self.exercise_empty_view_description = page.get_by_test_id(
